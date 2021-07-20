3dab2c9042f479922c871a0ebfe2c284c81aedae
Your Name <manishgusain66@gmail.com>
2021-07-20 21:43:50 +0530

    Allowed Host

--- encrypt_decrypt/settings.py
+++ encrypt_decrypt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vt-(@#v8#dref9htr%p26j20^+d$b_em@-x)vwzr(9t6ol+sxd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tocipher.herokuapp.com']
 
 
 # Application definition
